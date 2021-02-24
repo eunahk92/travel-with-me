2f2964eb9b235728cff3eef3d3daa3a75953def5
Eunah Kim <Eunahkim92@gmail.com>
2021-02-24 03:45:14 -0500

    Change file and folder structure

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#129793', '#FF7260', 'whitesmoke'],
+        fitToSection: true,
+        autoScrolling: true,
+        scrollHorizontally: true,
+        resetSliders: true,
+        // vertical pagination dots:
+        // navigation: true,
+        // navigationPosition: 'right',
+        slidesNavigation: true,
+        slidesNavPosition: "bottom",        
+    });
+});
