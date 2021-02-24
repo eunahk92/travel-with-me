473b03233bb9f30249adb2c19e8ffd0727aaebbe
Eunah Kim <Eunahkim92@gmail.com>
2021-02-24 00:05:32 -0500

    Feat navbar and stylings

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        sectionsColor: ['#f2f2f2', '#7BAABE', 'whitesmoke'],
+        sectionsColor: ['#129793', '#FF7260', 'whitesmoke'],
         autoScrolling:true,
+        navigation: true,
+        navigationPosition: "right",
+
     });
 });
+
+/* 
+Color scheme for site:
+	129793 - teal blue
+	505050 - gray
+	FFF5C3 - light yellow
+	9BD7D5 - light blue
+	FF7260 - salmon 
+*/
