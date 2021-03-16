140a10102856d320f376e939e55eaf925264ac02
Eunah Kim <Eunahkim92@gmail.com>
2021-03-16 15:39:16 -0400

    Add more trip details

--- assets/js/recommendations.js
+++ assets/js/recommendations.js
@@ -1,11 +1,11 @@
 const geocoder = new google.maps.Geocoder();
 let map, query;
 const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let labelIndex = 0;
+let markersAddyArr = [];
 const locationsArray = [
 	{
 		title: "Honolulu,Hawaii",
-		isInternational: false,
+		continent: 'North America',
 		coord: {
 			lat: 21.5010,
 			long: -158.0377136
@@ -96,9 +96,9 @@ const locationsArray = [
 			{
 				name: "Hanauma Bay",
 				address: "7455 Kalanianaʻole Hwy, Honolulu, HI 96825",
-				commentary: "One of the most famous places on the entire island for snorkeling. There is also Hanauma Bay Lookout if you don't want to go for a dip!",
+				commentary: "One of the most famous places on the entire island for Snorkel. There is also Hanauma Bay Lookout if you don't want to go for a dip!",
 				category: "toDo",
-				types: ["snorkeling", "beach"],
+				types: ["snorkel", "beach"],
 			},
 			{
 				name: "Hanauma Bay Lookout",
@@ -132,10 +132,200 @@ const locationsArray = [
 	},
 	{
 		title: "Iceland",
-		isInternational: true,
+		continent: "Europe",
 		coord: {
 			lat: 64.1420229,
 			long: -21.9265493
+		},
+		cityTips: ["Pack noodles and and go grocery shopping for snacks/ sandwiches: meat and to-go food is very expensive.", "Prepay for a gas card and use it to pump gas. Credit cards at gas stations will ask for a pin."],
+		recommendations: [
+			{
+				name: "Hallgrimskirkja Church",
+				where: "Reykjavik",
+				address: "Hallgrímstorg 1, 101 Reykjavík, Iceland",
+				commentary: "Beautiful church/ unique",
+				category: "toDo",
+				types: ["sightsee"],
+			},
+			{
+				name: "Blue Lagoon",
+				address: "Norðurljósavegur, Iceland",
+				commentary: "",
+				tips: ['PREBOOK YOUR TIX ONLINE for the date & time you want to visit! We booked the comfort package when we went (~$45).'],
+				category: "toDo",
+				types: ["spa"],
+			},
+			{
+				name: "Thingvellir National Park Silfra Fissure",
+				address: "Thingvellir, 801 Selfoss, Iceland",
+				commentary: "Snorkeling in Silfra Fissure is a MUST! Be prepared to go in water that is -16°(F). Silfra is said to have the clearest water in the world; feel free to have a sip of this pristine water at any point during your dive or snorkel (not kidding)!",
+				tips: ["Book through a company and they will provide wet suit and gear. I don't know if you can do it without booking a tour."],
+				category: "toDo",
+				types: ["snorkel", "hike"],
+			},
+			{
+				name: "Harpa Concert Hall",
+				address: "Austurbakki 2, 101 Reykjavík, Iceland",
+				commentary: "",
+				tips: [],
+				category: "toDo",
+				types: ["sightsee"],
+			},
+			{
+				name: "Gullfoss Waterfall",
+				address: "Gullfoss, Golden Circle, Iceland",
+				commentary: "If you're doing the Golden Circle Tour, this a recommended stop. It's like 2 draw dropping waterfalls into 1!",
+				tips: [],
+				category: "toDo",
+				types: ["sightsee"],
+			},
+			{
+				name: "Solheimasandur Plane Wreck",
+				address: "Vik, Iceland",
+				commentary: "Be prepared to walk! Google 'Parking for Sólheimasandur Plane Wreck' for better free parking but be prepared to walk ~45 mins to get to the plane wreck.",
+				tips: ["Pack snacks and drinks, long walk!"],
+				category: "toDo",
+				types: ["sightsee"],
+			},
+		]
+	},
+	{
+		title: "Seoul, Korea",
+		continent: "Asia",
+		coord: {
+			lat: 35.9078,
+			long: 127.7669
+		},
+		tips: [],
+		recommendations: [
+			{
+				name: "Dragon Hill Spa",
+				address: "40 Hangang-daero 21na-gil, Hangangno 3(sam)-ga, Yongsan-gu, Seoul, South Korea",
+				commentary: "One of the best in Seoul! Spa, sauna, golf course, & cinema all in one!",
+				tips: [],
+				category: "toDo",
+				types: ["spa"],
+			},
+			{
+				name: "Itaewon Land Sauna",
+				address: "",
+				commentary: "A less touristy jimjilbang (sauna). They have a big traditional oak wood sauna",
+				tips: [],
+				category: "toDo",
+				types: ["spa"],
+			},
+			{
+				name: "N Seoul Tower",
+				address: "105 Namsangongwon-gil, Yongsan 2(i)ga-dong, Yongsan-gu, Seoul, South Korea",
+				commentary: "Go on a clear day or at night for a great view of Seoul. You've probably seen the famous padlock bridge in K-dramas (& Netflix's To All The Boys: Always & Forever)!",
+				tips: ["PREBOOK your ticket online (~$9)! Gets busy/ packed, especially on weekends.", "I recommend taking the cable car up and taking the stairs down."],
+				category: "toDo",
+				types: ["sightsee"],
+			},
+			{
+				name: "Gyeongbokgung Palace",
+				address: "161 Sajik-ro, Jongno 1(il).2(i).3(sam).4(sa), Jongno-gu, Seoul, South Korea",
+				commentary: "",
+				tips: ["Make sure to see the changing of the guards' ceremony (done twice: 10am & 2pm - except Tuesdays).", "Outside of the palace are Hanbok shops where you can rent Hanboks by the hour or the whole day (you'll see many people do this and walk around/ in the palace)."],
+				category: "toDo",
+				types: ["sightsee"],
+			},
+			{
+				name: "Bukchon Hanok Village",
+				address: "",
+				commentary: "Well preserved, traditional, Korean village: taking a walk around the village is like being transported back in time.",
+				tips: ["Stop by the Tourist Info Center and get a map (shows you all the main points to see).", ""],
+				category: "toDo",
+				types: ["sightsee"],
+			},
+			{
+				name: "Lotte World",
+				address: "",
+				commentary: "It's like Disney world but not Disney. A day's worth activities: world's largest indoor theme park, shopping malls, outdoor amusement park, sports facilities, and more!",
+				tips: [],
+				category: "toDo",
+				types: ["sightsee", "shop", "eat"],
+			},
+			{
+				name: "Bukhansan National Park",
+				address: "262 Bogukmun-ro, Jeongneung 4(sa)-dong, Seongbuk-gu, Seoul, South Korea",
+				commentary: "Hike up Bukhansan Mountain (follow markers for levels of hike).",
+				tips: [],
+				category: "toDo",
+				types: ["hike"],
+			},
+			{
+				name: "Myeongdong Market",
+				address: "Myeong-dong, Jung-gu, Seoul, South Korea",
+				commentary: "Myeongdong Market is one of South Korea’s prime shopping districts – you’ll find department stores, shopping malls, and independent shops, together with a host of trendy restaurants and cafes.",
+				tips: [],
+				category: "toDo",
+				types: ["shop", "eat"],
+			},
+			{
+				name: "Coex Mall",
+				address: "",
+				commentary: "Korea's largest underground shopping mall!",
+				tips: [],
+				category: "",
+				types: [""],
+			},
+			{
+				name: "Starfield Coex Library",
+				address: "513, Yeongdong-daero, Gangnam-gu Starfield COEX Mall, Seoul 06164 South Korea",
+				commentary: "Inside Coex Mall: THE most beautiful library EVER. Over 50,000 books and 2 stories. You've probably seen this library in K-dramas (like Record of Youth).",
+				tips: [],
+				category: "toDo",
+				types: ["sightsee", "shop"],
+			},
+			{
+				name: "Seoul City Wall Trail",
+				address: "",
+				commentary: "Usually seen in K-Dramas where they go and sit on the wall! 'The Fortress Wall of Seoul' with a skyline view of the city and mountains. Recommend going during sunset.",
+				tips: [],
+				category: "toDo",
+				types: ["sightsee"],
+			},
+			{
+				name: "Namdaemun Market",
+				address: "21, Namdaemunsijang 4-gil, Jung-gu, Seoul",
+				commentary: "Largest traditional market",
+				tips: ["FYI market is closed on Sunday"],
+				category: "toDo",
+				types: ["shop", "eat"],
+			},
+			{
+				name: "Changdeok Palace",
+				address: "",
+				commentary: "Part of the Five Grand Palaces made by the Joseon Dynasty King. The rear garden at the palace was done to create the ideal resting place for the royal family members.",
+				tips: ["PREBOOK tickets online to avoid lines (~2.50)"],
+				category: "toDo",
+				types: ["sightsee"],
+			},
+			{
+				name: "Banpo Bridge",
+				address: "Banpo 2(i)-dong, Seoul, South Korea",
+				commentary: "2 tier bridge over the famous Han River.",
+				tips: ["Watch the Rainbow Fountain Show (best location for best view is from Banpo Hangang Park!): Check the times online (usually once at noon and a few at night)."],
+				category: "toDo",
+				types: ["sightsee"],
+			},
+			{
+				name: "Mugyodong Bugeokukjib",
+				address: "38 Eulji-ro 1-gil, Mugyo-dong, Jung-gu, Seoul, South Korea",
+				commentary: "Very unique experience: They serve only one local dish per day, there is no menu and the meal comes with many small side dishes to combine with the entree.",
+				tips: [],
+				category: "extra",
+				types: ["eat"],
+			},
+		]
+	},
+	{
+		title: "Dubai, UAE",
+		continent: "Asia",
+		coord: {
+			lat: 24.4539,
+			long: 54.3773
 		},
 		recommendations: [
 			{
@@ -143,34 +333,56 @@ const locationsArray = [
 				where: "Reykjavik",
 				address: "Hallgrímstorg 1, 101 Reykjavík, Iceland",
 				commentary: "Beautiful church/ unique",
-				category: "toDo"
-			},
-		]
-	}
-];
-
-initMap = () => {
-	map = new GMaps({
-		div: '#map',
-		zoom: 1.5,
-		lat: 28.5383,
-		lng: -99.3792
-	});
-
-	locationsArray.forEach(city => {
-		map.addMarker({
-			lat: city.coord.lat,
-			lng: city.coord.long,
-			title: city.title,
-			click: function(e) {
-				renderRecommendationsPage(e.title);
+				tips: [],
+				category: "toDo",
+				types: ["sightsee"],
 			}
-		});
-	})
-}
+		]
+	},
+	{
+		title: "Italy, Rome",
+		continent: "Europe",
+		coord: {
+			lat: 41.9028,
+			long: 12.4964
+		},
+		recommendations: [
+			{
+				name: "Hallgrimskirkja Church",
+				where: "Reykjavik",
+				address: "Hallgrímstorg 1, 101 Reykjavík, Iceland",
+				commentary: "Beautiful church/ unique",
+				tips: [],
+				category: "toDo",
+				types: ["sightsee"],
+			}
+		]
+	},
+	{
+		title: "Orlando, Florida",
+		continent: "North America",
+		coord: {
+			lat: 28.5383,
+			long: 81.3792
+		},
+		recommendations: [
+			{
+				name: "Hallgrimskirkja Church",
+				where: "Reykjavik",
+				address: "Hallgrímstorg 1, 101 Reykjavík, Iceland",
+				commentary: "Beautiful church/ unique",
+				tips: [],
+				category: "toDo",
+				types: ["sightsee"],
+			}
+		]
+	},
+	
+];
 
 async function renderMap(array, location) {
 	try {
+		let labelIndex = 0;
 		await locationsArray.forEach(city => {
 			if (city.title == location) {
 				map.removeMarkers();
@@ -184,20 +396,22 @@ async function renderMap(array, location) {
 				if (status == google.maps.GeocoderStatus.OK) {
 					let lat = results[0].geometry.location.lat();
 					let long = results[0].geometry.location.lng();
-					let marker = map.addMarker({
+					let markerLabel = labels[labelIndex++ % labels.length];
+					map.addMarker({
 						lat: lat,
 						lng: long,
 						title: spot,
-						label: labels[labelIndex++ % labels.length],
+						label: markerLabel,
 						infoWindow: {
 							content: spot
 						},
 						click: function(e) {
 							let latLng = new google.maps.LatLng(lat, long);
-							map.setZoom(14);
+							map.setZoom(13);
 							map.panTo(latLng);
 						}
 					});
+					markersAddyArr.push({ location, spot, markerLabel });
 				} 
 			}); 
 		});
@@ -210,12 +424,19 @@ renderGoogleLinks = (address) => {
 	return link;
 };
 
+getMarkerLabel = (something) => {
+	for (let i = 0; i < markersAddyArr.length; i++) {
+			console.log(true)
+	}
+}
+
 renderListOfRecommendations = (array) => {
 	array.forEach(spot => {
 		let { name, address, types, category } = spot;
 		let query = renderGoogleLinks(address);
 		let icons = renderIcons(types);
 		icons = icons.toString().replaceAll(',', '');
+		getMarkerLabel(address);
 		let li = `
 			<div class="row d-flex justify-content-start m-0 p-0">
 				<a href="${query}" target="_blank" class="recommendationLink mr-3">
@@ -229,6 +450,9 @@ renderListOfRecommendations = (array) => {
 			case "toDo":
 				$("#tripContent-adventures-list").append(li);
 				break;
+			case "extra":
+				$("#tripContent-extra-list").append(li);
+				break;
 		};
 	})
 };
@@ -236,24 +460,27 @@ renderListOfRecommendations = (array) => {
 renderLocationLinks = () => {
 	locationsArray.forEach(location => {
 		const locationLink = `
-			<a class="locationLink" data-bs-toggle="collapse" href="#tripContentCollapse" role="button" aria-expanded="false" aria-controls="tripContentCollapse">
-				<small class="px-1 text-center">
+			<a class="locationLink"><small>
 					${location.title}
-				</small>
-			</a>
+			</small></a>
 		`
-		location.isInternational ? $(".europeLocations").append(locationLink) : $(".usLocations").append(locationLink);
+		if (location.continent === 'North America') {
+			$('.usLocations').append(locationLink);
+		} else if (location.continent === 'Europe') {
+			$('.europeLocations').append(locationLink);
+		} else {
+			$('.asiaLocations').append(locationLink);
+		}
 	});
 };
 
 renderLocationContent = (location) => {
 	$('#locationTitle').attr('data-city', location);
-	$("#tripContent-eateries-list").empty();
-	$("#tripContent-adventures-list").empty();
-	$("#tripContent-extra-list").empty();
+	$('#tripContent-eateries-list').empty();
+	$('#tripContent-adventures-list').empty();
+	$('#tripContent-extra-list').empty();
 	let markers = [];
 	let res = locationsArray.filter(city => city.title == location);
-	console.log(res);
 	const { title, recommendations } = res[0];
 	recommendations.forEach(spot => {
 		if (spot.address) {
@@ -269,47 +496,81 @@ renderIcons = (array) => {
 	let icons = [];
 	array.forEach(type => {
 		switch (type) {
-			case "seafood":
+			case 'seafood':
 				icons.push(`<i class="fas fa-fish"></i>`);
 				break;
-			case "dessert":
+			case 'dessert':
 				icons.push(`<i class="fas fa-ice-cream"></i>`);
 				break;
-			case "hike":
+			case 'hike':
 				icons.push(`<i class="fas fa-hiking"></i>`);
 				break;
-			case "shop":
+			case 'shop':
 				icons.push(`<i class="fas fa-shopping-bag"></i>`);
 				break;
-			case "eat":
+			case 'eat':
 				icons.push(`<i class="fas fa-utensils"></i>`);
 				break;
-			case "snorkeling":
+			case 'snorkel':
 				icons.push(`<i class="fas fa-swimmer"></i>`);
 				break;
-			case "bar":
+			case 'bar':
 				icons.push(`<i class="fas fa-glass-martini-alt"></i>`);
+				break;
+			case 'sightsee': 
+				icons.push(`<i class="fas fa-camera-retro"></i>`);
 				break;
 		}	
 	})
 	return icons;
 };
 
+renderMarkerLabelsForLi = () => {
+	return `<div class="numberCircle"></div>`
+};
+
 renderRecommendationsPage = (location) => {
 	let dataValue = $('#locationTitle').attr('data-city');
-	if ($("#tripContent").css('display') == 'none') {
-		$("#tripContent").css('display', 'block');
+	if ($('#tripContent').css('display') == 'none') {
+		$('#tripContent').css('display', 'block');
 		fullpage_api.moveSectionDown();
 		renderLocationContent(location);
+		let scrollHTML = `
+			<div id="scrollDownDirectionDiv" class="mb-5">
+				<p class="verticalText">scroll down</p>
+				<i class="fas fa-angle-double-down fa-2x"></i>
+			</div>
+		`
+		$('#recommendationsSection').append(scrollHTML);
 	} 
-	if ($("#tripContent").css('display') == 'block' && dataValue == location) {
+	if ($('#tripContent').css('display') == 'block' && dataValue == location) {
 		fullpage_api.moveTo(2);
 	}
-	if ($("#tripContent").css('display') == 'block' && dataValue != location) {	
-		$("#tripContent").css('display', 'block');
+	if ($('#tripContent').css('display') == 'block' && dataValue != location) {	
 		fullpage_api.moveSectionDown();
 		renderLocationContent(location);
 	} 
+};
+
+initMap = () => {
+	map = new GMaps({
+		div: '#map',
+		zoom: 1.5,
+		lat: 28.5383,
+		lng: -99.3792
+	});
+
+	locationsArray.forEach(city => {
+		map.addMarker({
+			lat: city.coord.lat,
+			lng: city.coord.long,
+			title: city.title,
+			label: city.recommendations.length.toString(),
+			click: function(e) {
+				renderRecommendationsPage(e.title);
+			}
+		});
+	})
 };
 
 $(document).ready(() => {
