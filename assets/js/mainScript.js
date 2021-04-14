1eda20b202173fa5b2839f61a255306959d268de
Eunah Kim <Eunahkim92@gmail.com>
2021-04-14 00:17:06 -0400

    Added anchors

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         sectionsColor: ['#1F858F', '#F24C4E', 'rgb(255,204,103)'],
-        fitToSection: true,
+        anchors: ['about-me', 'recommendations', 'details', 'pictures'],
+        fitToSection: false,
         autoScrolling: false,
         slidesNavigation: true,
         slidesNavPosition: "bottom",      
         menu: true,
         normalScrollElements: '#tripContent',
         normalScrollElements: '#map',
-        scrollOverflow: true,
     });
 });
