81cddb4d4670268869e98889de788b3293243c65
Eunah Kim <Eunahkim92@gmail.com>
2021-04-14 00:34:47 -0400

    Add more properties for fullpage

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
+    let windowsHeight = $('.universal-container').height();
     $('#fullpage').fullpage({
         sectionsColor: ['#1F858F', '#F24C4E', 'rgb(255,204,103)'],
         anchors: ['about-me', 'recommendations', 'details', 'pictures'],
+        css3: true,
         fitToSection: false,
         autoScrolling: false,
         slidesNavigation: true,
@@ -9,5 +11,6 @@ $(document).ready(function() {
         menu: true,
         normalScrollElements: '#tripContent',
         normalScrollElements: '#map',
+        responsiveHeight: windowsHeight
     });
 });
